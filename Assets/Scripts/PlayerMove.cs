10b1595446d0410c8c8b9a68d08591000fbeecfb
Teemu Spoof <steemum@hotmail.com>
2023-01-31 16:32:51 +0200

    Added very basic movement

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    private Rigidbody2D         m_body2d;
+    private Rigidbody2D	m_body2d;
+    private Vector2		positions;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,13 +15,19 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+		positions = transform.position;
         // Get movement direction (-1 left, 1 right)
         float inputRawX = Input.GetAxisRaw("Horizontal");
         float inputRawY = Input.GetAxisRaw("Vertical");
-        // if (inputRawX != 0.0f)
-        // {
-        //     // Player position
-        //     position = transform.Translate 
-        // }
+        if (inputRawX != 0.0f)
+        {
+            // Move player horizontaly
+            transform.Translate(new Vector2(inputRawX * Time.deltaTime * 6, 0));
+        }
+		if (inputRawY != 0.0f)
+        {
+            // Move player horizontaly
+            transform.Translate(new Vector2(0, inputRawY * Time.deltaTime * 6));
+        }
     }
 }
