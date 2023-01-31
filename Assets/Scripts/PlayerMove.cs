724a3cb1b076a9ee156307af13aa7ade453b8274
Kimpozer <86662547+composerjunhee@users.noreply.github.com>
2023-01-31 22:25:47 +0200

    Revert "Merge branch 'main' of https://github.com/composerjunhee/GGJ_RE_ATJ"
    This reverts commit d64b6b351f1705761aaf080fb7a72e8b8a0f77fa, reversing
    changes made to b4f7059927b4cb441cc6f83872e948d895286c1c.

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,12 +4,8 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-	public Animator			animator;
-	public SpriteRenderer	spriteRenderer;
-    public Rigidbody2D		m_body2d;
-    private Vector2			positions;
-
-	private Vector3 m_Velocity = Vector3.zero;
+    private Rigidbody2D	m_body2d;
+    private Vector2		positions;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,28 +21,13 @@ public class PlayerMove : MonoBehaviour
         float inputRawY = Input.GetAxisRaw("Vertical");
         if (inputRawX != 0.0f)
         {
-			Debug.Log(m_body2d.velocity);
-			
-			if (inputRawX == -1)
-				spriteRenderer.flipX = false;
-			if (inputRawX == 1)
-				spriteRenderer.flipX = true;
             // Move player horizontaly
-            // transform.Translate(new Vector2(inputRawX * Time.deltaTime * 6, 0));
-
-			// Move the character by finding the target velocity
-			Vector3 targetVelocity = new Vector2(inputRawX * 10f, m_body2d.velocity.y);
-			// And then smoothing it out and applying it to the character
-			m_body2d.velocity = Vector3.SmoothDamp(m_body2d.velocity, targetVelocity, ref m_Velocity, 0.1f); // Like skating on ice.. No good
-
+            transform.Translate(new Vector2(inputRawX * Time.deltaTime * 6, 0));
         }
 		if (inputRawY != 0.0f)
         {
             // Move player horizontaly
-			if (inputRawY == 1)
-				m_body2d.AddForce(new Vector2(0f, 6f)); // Can fly
-            // transform.Translate(new Vector2(0, inputRawY * Time.deltaTime * 6));
+            transform.Translate(new Vector2(0, inputRawY * Time.deltaTime * 6));
         }
-		animator.SetFloat("Speed", Mathf.Abs(m_body2d.velocity.x));
     }
 }
