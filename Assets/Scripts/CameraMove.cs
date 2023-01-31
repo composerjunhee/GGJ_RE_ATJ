8c27f9f0f3705a1e9e0d7fccb0e08ed618dd7233
affmde <affmde@hotmail.com>
2023-01-31 16:34:33 +0200

    basic camera move added

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+	public Transform followTransform;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void FixedUpdate()
+	{
+		this.transform.position = new Vector3(followTransform.position.x, followTransform.position.y, this.transform.position.z);
+	}
+}
