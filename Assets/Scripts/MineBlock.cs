1e81f1c78caef0aeac858c1d7e1f20ba81c22680
Teemu Spoof <steemum@hotmail.com>
2023-01-31 15:26:14 +0200

    Made MineBlock to work with Tiles

--- Assets/Scripts/MineBlock.cs
+++ Assets/Scripts/MineBlock.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class MineBlock : MonoBehaviour
 {
-    private Vector2 position;
-    private float rayDistance = 0.5f;
+    private Vector2		position;
+    private float		rayDistance = 0.5f;
+	public Tilemap		map;
+	private Vector3Int	cellPosition;
+	private TileBase	tile;
+	private float		inputRawX;
+	private float		inputRawY;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,24 +22,28 @@ public class MineBlock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+		// Player position
+		position = transform.position;
         // Get movement direction (-1 left, 1 right)
-        float inputRaw = Input.GetAxisRaw("Horizontal");
-        if (inputRaw != 0.0f)
+        inputRawX = Input.GetAxisRaw("Horizontal");
+		// Get movement direction (-1 down, 1 up)
+		inputRawY = Input.GetAxisRaw("Vertical");
+        if (inputRawX != 0.0f)
         {
-            // Player position
-            position = transform.position;
-            // Checks if there is a object next to the player on the horizontal axis 
-            RaycastHit2D hit = Physics2D.Raycast(
-                position + new Vector2(0.66f / 2 * inputRaw, 1.3125f / 2), // TODO: check player edges and use those to set the origin vectoryou
-                Vector2.right * inputRaw,
-                rayDistance);
-            // Check if the object is minable
-            if (hit.collider != null && hit.collider.tag == "Minable")
-                Destroy(hit.collider.gameObject, 0);
-
-
-            Debug.Log("Is block: " + hit.collider.name);
-            Debug.Log("Position: " + position);
+            // Checks if there is a tile next to the player on the horizontal axis 
+            cellPosition = map.WorldToCell(position + new Vector2(0.6f * inputRawX, 0));	
+			tile = map.GetTile(cellPosition);	
         }
+		if (inputRawY != 0.0f)
+        {
+			// Checks if there is a tile next to the player on the vertical axis 
+			cellPosition = map.WorldToCell(position + new Vector2(0, 1.1f * inputRawY));
+			tile = map.GetTile(cellPosition);
+        }
+		if (tile != null)
+		{
+			Debug.Log("tile: " + tile);
+			map.SetTile(cellPosition, null);
+		}
     }
 }
