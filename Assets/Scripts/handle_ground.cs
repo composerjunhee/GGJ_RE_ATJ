73b1ceb76d4b5702a781cac21147351701751a4a
affmde <affmde@hotmail.com>
2023-01-31 15:34:40 +0200

    background, tree and tilemaps added

---
+++ Assets/Scripts/handle_ground.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+public class handle_ground : MonoBehaviour
+{
+	public Tilemap map;
+	private Vector3Int position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
