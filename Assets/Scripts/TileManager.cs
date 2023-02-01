e9ca62e5a7b6fd4be9584800a6347611672930ea
affmde <affmde@hotmail.com>
2023-02-01 16:48:33 +0200

    some changes added to TileManager

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -6,10 +6,24 @@ public class TileManager : MonoBehaviour
 {
 	[SerializeField]
 	private Tilemap map;
-
+	private BoundsInt bounds;
 	private Dictionary<Vector3Int, int> strengthTiles = new Dictionary<Vector3Int, int>();
-    
-	private void	change_strenght(Vector3Int gridPosition)
+    private TileBase[] allTiles;
+	/*private	void	Get_tiles()
+	{
+		bounds = map.cellBounds;
+		allTiles = map.GetTilesBlock(bounds);
+		for (int x = 0; x < bounds.size.x; x++)
+		{
+			for (int y = 0; y < bounds.size.y; y++)
+			{
+				TileBase tile = allTiles[x + y * bounds.size.x];
+				print("tile name: " + tile.name);
+			}
+		}
+		
+	}*/
+	public void	change_strenght(Vector3Int gridPosition)
 	{
 		if (!strengthTiles.ContainsKey(gridPosition))
 			strengthTiles.Add(gridPosition, Random.Range(3, 7));
@@ -26,6 +40,19 @@ public class TileManager : MonoBehaviour
 		}
 	}
 
+	/*private void	ShrinkTile()
+	{
+		int	x = bounds.size.x;
+		int	y = bounds.size.y;
+
+
+		print("first tile is: " + allTiles[0]);
+	}*/
+
+	/*private void Awake()
+	{
+		Get_tiles();
+	}*/
 	private void	Update()
 	{
 		if (Input.GetMouseButtonDown(0))
@@ -33,6 +60,7 @@ public class TileManager : MonoBehaviour
 			Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			Vector3Int gridPosition = map.WorldToCell(mousePosition);
 			change_strenght(gridPosition);
+			//ShrinkTile();
 		}
 	}
     
