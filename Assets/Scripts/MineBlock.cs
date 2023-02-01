2ff5d89095d7653665d1dabb0ff6137dd4a40a06
Teemu Spoof <steemum@hotmail.com>
2023-02-01 16:15:49 +0200

    Made the end of the mining animation to actually destroy the block

--- Assets/Scripts/MineBlock.cs
+++ Assets/Scripts/MineBlock.cs
@@ -6,14 +6,15 @@ using UnityEngine.Tilemaps;
 public class MineBlock : MonoBehaviour
 {
     private Vector2		position;
-    private float		rayDistance = 0.5f;
 	public Tilemap		map;
 	private Vector3Int	cellPosition;
 	private TileBase	tile;
 	private float		inputRawX;
 	private float		inputRawY;
-
+    private float		mineDirectionX;
+	private float		mineDirectionY;
     private Animator	animator;
+	private bool		mining = false;
 
     // Start is called before the first frame update
     void Start()
@@ -21,37 +22,57 @@ public class MineBlock : MonoBehaviour
 		animator = GetComponent<Animator>();
     }
 
+	public void finnishMining()
+	{
+		mining = false;
+        // Player position
+        position = transform.position;
+        if (mineDirectionX != 0.0f)
+        {
+            // Checks if there is a tile next to the player on the horizontal axis 
+            cellPosition = map.WorldToCell(position + new Vector2(0.6f * mineDirectionX, 0));	
+            tile = map.GetTile(cellPosition);	
+        }
+        if (mineDirectionY != 0.0f)
+        {
+            // Checks if there is a tile next to the player on the vertical axis 
+            cellPosition = map.WorldToCell(position + new Vector2(0, 1.1f * mineDirectionY));
+            tile = map.GetTile(cellPosition);
+        }
+        // If there is a tile mine it off
+        if (tile != null)
+        {
+            Debug.Log("tile: " + tile);
+            map.SetTile(cellPosition, null);
+        }
+		animator.SetBool("Mining", mining);
+	}
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Fire1"))
+        if (mining == false)
         {
-            // Player position
-            position = transform.position;
             // Get movement direction (-1 left, 1 right)
             inputRawX = Input.GetAxisRaw("Horizontal");
             // Get movement direction (-1 down, 1 up)
             inputRawY = Input.GetAxisRaw("Vertical");
-			animator.SetBool("Mining", true);
             if (inputRawX != 0.0f)
             {
-                // Checks if there is a tile next to the player on the horizontal axis 
-                cellPosition = map.WorldToCell(position + new Vector2(0.6f * inputRawX, 0));	
-                tile = map.GetTile(cellPosition);	
+                mineDirectionX = inputRawX;
+                mineDirectionY = 0f;
             }
             if (inputRawY != 0.0f)
             {
-                // Checks if there is a tile next to the player on the vertical axis 
-                cellPosition = map.WorldToCell(position + new Vector2(0, 1.1f * inputRawY));
-                tile = map.GetTile(cellPosition);
-            }
-            if (tile != null)
-            {
-                Debug.Log("tile: " + tile);
-                map.SetTile(cellPosition, null);
+                mineDirectionY = inputRawY;
+                mineDirectionX = 0f;
             }
         }
-		else
-			animator.SetBool("Mining", false);
+        if (Input.GetButton("Fire1") && mining == false)
+        {
+			Debug.Log("Mining...");
+			mining = true;
+			animator.SetBool("Mining", mining);
+        }
     }
 }
