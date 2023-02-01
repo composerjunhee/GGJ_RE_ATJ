9b5fe8056b8e0b0db439309f1046fc4acfe0ccd1
Teemu Spoof <steemum@hotmail.com>
2023-02-01 14:00:37 +0200

    Added Mining animation

--- Assets/Scripts/MineBlock.cs
+++ Assets/Scripts/MineBlock.cs
@@ -13,37 +13,45 @@ public class MineBlock : MonoBehaviour
 	private float		inputRawX;
 	private float		inputRawY;
 
+    private Animator	animator;
+
     // Start is called before the first frame update
     void Start()
     {
-
+		animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-		// Player position
-		position = transform.position;
-        // Get movement direction (-1 left, 1 right)
-        inputRawX = Input.GetAxisRaw("Horizontal");
-		// Get movement direction (-1 down, 1 up)
-		inputRawY = Input.GetAxisRaw("Vertical");
-        if (inputRawX != 0.0f)
+        if (Input.GetButton("Fire1"))
         {
-            // Checks if there is a tile next to the player on the horizontal axis 
-            cellPosition = map.WorldToCell(position + new Vector2(0.6f * inputRawX, 0));	
-			tile = map.GetTile(cellPosition);	
+            // Player position
+            position = transform.position;
+            // Get movement direction (-1 left, 1 right)
+            inputRawX = Input.GetAxisRaw("Horizontal");
+            // Get movement direction (-1 down, 1 up)
+            inputRawY = Input.GetAxisRaw("Vertical");
+			animator.SetBool("Mining", true);
+            if (inputRawX != 0.0f)
+            {
+                // Checks if there is a tile next to the player on the horizontal axis 
+                cellPosition = map.WorldToCell(position + new Vector2(0.6f * inputRawX, 0));	
+                tile = map.GetTile(cellPosition);	
+            }
+            if (inputRawY != 0.0f)
+            {
+                // Checks if there is a tile next to the player on the vertical axis 
+                cellPosition = map.WorldToCell(position + new Vector2(0, 1.1f * inputRawY));
+                tile = map.GetTile(cellPosition);
+            }
+            if (tile != null)
+            {
+                Debug.Log("tile: " + tile);
+                map.SetTile(cellPosition, null);
+            }
         }
-		if (inputRawY != 0.0f)
-        {
-			// Checks if there is a tile next to the player on the vertical axis 
-			cellPosition = map.WorldToCell(position + new Vector2(0, 1.1f * inputRawY));
-			tile = map.GetTile(cellPosition);
-        }
-		if (tile != null)
-		{
-			Debug.Log("tile: " + tile);
-			map.SetTile(cellPosition, null);
-		}
+		else
+			animator.SetBool("Mining", false);
     }
 }
