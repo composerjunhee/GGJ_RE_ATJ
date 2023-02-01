ab3680bf2c5266a021dde058708b986896954070
affmde <affmde@hotmail.com>
2023-02-01 08:57:39 +0200

    revert commit

---
+++ Assets/Scripts/pickup.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickup : MonoBehaviour
+{
+    public GameObject slotItem;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("Player"))
+        {
+            inventory inven = collision.GetComponent<inventory>();
+            for (int i = 0; i < inven.slots.Count; i++)
+            {
+                if(inven.slots[i].isEmpty)
+                {
+                    Instantiate(slotItem, inven.slots[i].slotObj.transform, false);
+                    inven.slots[i].isEmpty = false;
+                    Destroy(this.gameObject);
+                    break;
+                }
+            }
+        }
+    }
+}
