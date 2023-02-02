483e5abb5b1e71b23d2dd46d9e2343f242037727
Teemu Spoof <steemum@hotmail.com>
2023-02-02 18:40:41 +0200

    added jumping animation + stuff

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -11,15 +11,18 @@ public class PlayerMove : MonoBehaviour
     public float speed = 7f;
     public float jumpingPower = 8f;
     private bool isFacingRight = true;
-
     public Tilemap		map;
 	private Vector3Int	cellPosition;
 	private TileBase	tile;
+    private Animator	animator;
+    private bool        inAir = false;
+    private int         landing_delay = 5; // Number of frames delay. Hacky fix for landing bug 
 
     void Start()
     {
 		rb = GetComponent<Rigidbody2D>();
         boxCollider2D = GetComponent<BoxCollider2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -32,8 +35,21 @@ public class PlayerMove : MonoBehaviour
         // if (Input.GetButtonDown("Jump") && !IsGrounded())
         //     rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
         if (Input.GetButtonDown("Jump") && IsGrounded())
-            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
-
+        {
+            animator.SetTrigger("Jump");
+            // rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+        // landed after jump
+        if (inAir && IsGrounded())
+        {
+            landing_delay--;
+            if (landing_delay == 0)
+            {
+                animator.SetTrigger("Landed");
+                inAir = false;
+                landing_delay = 5;
+            }
+        }
     }
 
     private void FixedUpdate()
@@ -42,9 +58,15 @@ public class PlayerMove : MonoBehaviour
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
     }
 
+    private void Jumped()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+    	animator.SetTrigger("Flying");
+		inAir = true;
+    }
     private bool IsGrounded()
     {
-        cellPosition = map.WorldToCell(boxCollider2D.bounds.center - new Vector3(0, boxCollider2D.bounds.extents.y + 0.1f, 0));
+        cellPosition = map.WorldToCell(boxCollider2D.bounds.center - new Vector3(0, boxCollider2D.bounds.extents.y + 0.05f, 0));
         
         tile = map.GetTile(cellPosition);
         if (tile)
