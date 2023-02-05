965f6e03a62c466513a33cae23cf1e659729b862
Kimpozer <86662547+composerjunhee@users.noreply.github.com>
2023-02-05 12:20:55 +0200

    RandomVoicePlayer

---
+++ Assets/Scripts/audioTest.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioTest : MonoBehaviour
+{
+    private AudioSource theAudio;
+
+    [SerializeField] private AudioClip[] clip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        theAudio = GetComponent<AudioSource>();
+    }
+
+    public void PlaySE()
+    {
+        int _temp = Random.Range(0, 6);
+
+        theAudio.clip = clip[_temp];
+        theAudio.Play();
+    }
+}
